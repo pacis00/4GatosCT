c6427ce844bbc819500b622c7bf0ef3785d4d688
pacis <pacis@192.168.0.100>
2021-12-04 19:17:44 +0100

    Initial commit. creacion del proyecto back

---
+++ 4gatos/src/main/java/cuatrogatosct/com/gatos/CuatroGatosCartagenaApplication.java
@@ -0,0 +1,13 @@
+package cuatrogatosct.com.gatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuatroGatosCartagenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuatroGatosCartagenaApplication.class, args);
+	}
+
+}
---
+++ 4gatos/src/test/java/cuatrogatosct/com/gatos/CuatroGatosCartagenaApplicationTests.java
@@ -0,0 +1,13 @@
+package cuatrogatosct.com.gatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuatroGatosCartagenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
